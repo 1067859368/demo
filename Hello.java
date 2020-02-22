0a4436ba58c21298814693ac97865f3b98657144
xiaofei <cauliubin@163.com>
2020-02-22 16:40:46 +0800

    hello

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("2Hello World");
+		 System.out.println("1Hello World");
+		  System.out.println("3Hello World");
+		   System.out.println("4Hello World");
+		    System.out.println("5Hello World");
+
+    }
+}
